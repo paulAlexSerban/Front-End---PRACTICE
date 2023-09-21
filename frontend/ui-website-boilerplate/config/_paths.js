b9b93c5f5f732a99ea294fe16675aa6168227ab4
paulAlexSerban <paul.alex.serban@gmail.com>
2023-09-21 11:34:28 +0300

    feat(ui-app-boilerplate): added base fe build system

---
+++ frontend/ui-website-boilerplate/config/_paths.js
@@ -0,0 +1,6 @@
+const path = require('path');
+
+const DIST_DIR = path.resolve(__dirname, '..', 'dist');
+const SRC_DIR = path.resolve(__dirname, '..', 'source');
+
+module.exports = { DIST_DIR, SRC_DIR };
---
+++ frontend/ui-website-boilerplate/config/index.js
@@ -0,0 +1,4 @@
+const { NODE_ENV } = require('./_env');
+const { DIST_DIR, SRC_DIR } = require('./_paths');
+
+module.exports = { NODE_ENV, DIST_DIR, SRC_DIR };
