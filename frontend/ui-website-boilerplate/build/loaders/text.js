624087b55acf4f1b698e35565f3ee53d2a2adf4f
paulAlexSerban <paul.alex.serban@gmail.com>
2023-09-21 11:20:32 +0300

    style(formats): used prettier

--- frontend/ui-website-boilerplate/build/loaders/text.js
+++ frontend/ui-website-boilerplate/build/loaders/text.js
@@ -1,5 +1,5 @@
-module.exports =             {
-  // use asset modules to handle text assets
-  test: /\.(txt)$/,
-  type: 'asset/source',
+module.exports = {
+    // use asset modules to handle text assets
+    test: /\.(txt)$/,
+    type: 'asset/source',
 };
